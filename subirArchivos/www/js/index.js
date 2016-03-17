2f34d480bdce042d85dd739dd728a1b2e0cc35eb
nicoDSR <nico.uy@gmail.com>
2016-03-17 11:26:49 -0300

    subir archivos desde el teléfono al servidor

---
+++ subirArchivos/www/js/index.js
@@ -0,0 +1,141 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+var pictureSource; // picture source
+var destinationType; // sets the format of returned value
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+// device APIs are available
+//
+
+function onDeviceReady() {
+    app.onDeviceReady();
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+}
+// Called when a photo is successfully retrieved
+//
+
+function onPhotoDataSuccess(imageURI) {
+	
+    // Uncomment to view the base64-encoded image data
+    console.log(imageURI);
+    // Get image handle
+    //
+    var cameraImage = document.getElementById('image');
+    // Unhide image elements
+    //
+    cameraImage.style.display = 'block';
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    cameraImage.src = imageURI;
+}
+// Called when a photo is successfully retrieved
+//
+
+function onPhotoURISuccess(imageURI) {
+	
+    // Uncomment to view the image file URI
+    console.log(imageURI);
+    // Get image handle
+    //
+    var galleryImage = document.getElementById('image');
+    // Unhide image elements
+    //
+    galleryImage.style.display = 'block';
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    galleryImage.src = imageURI;
+}
+// A button will call this function
+//
+
+function capturePhoto() {
+    // Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality: 30,
+        targetWidth: 600,
+        targetHeight: 600,
+        destinationType: destinationType.FILE_URI,
+        saveToPhotoAlbum: true
+    });
+}
+// A button will call this function
+//
+
+function getPhoto(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        quality: 30,
+        targetWidth: 600,
+        targetHeight: 600,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source
+    });
+}
+// Called if something bad happens.
+//
+
+function onFail(message) {
+    //alert('Failed because: ' + message);
+}
+
+function upload() {
+    console.log("subiendo?");
+    var img = document.getElementById('image');
+    var imageURI = img.src;
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+    options.mimeType = "video/mp4";
+    var params = new Object();
+    options.params = params;
+    options.chunkedMode = false;
+    var ft = new FileTransfer();
+    ft.upload(imageURI, "http://192.168.1.104:8081/subir", win, fail,
+        options);
+}
+
+function win(r) {
+    console.log("Code = " + r.responseCode);
+    console.log("Response = " + r.response);
+    console.log("Sent = " + r.bytesSent);
+}
+
+function fail(error) {
+    alert("An error has occurred: Code = " + error.code);
+    console.log("upload error source " + error.source);
+    console.log("upload error target " + error.target);
+}
